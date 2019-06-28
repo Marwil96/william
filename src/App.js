74cf90498dbc3b45a056791dff1d4464a8b1c709
William_MAC <william_martinsson@hotmail.com>
2019-06-29 00:57:44 +0200

    googlecrawler help polyfill

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import 'babel-polyfill'
 import { BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 
