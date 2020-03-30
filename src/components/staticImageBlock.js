f502a5aadbfb86b455958c36c36405d7204aca23
Marwil96 <william_martinsson@hotmail.com>
2020-03-30 13:05:23 +0200

    hopefully fixes the sticky bug

--- src/components/staticImageBlock.js
+++ src/components/staticImageBlock.js
@@ -31,7 +31,9 @@ const StaticImageBlock = ({id, active}) => {
 `} render={data => {
   const image = data.allImageSharp.nodes.filter(item => item.resize.originalName === id);
    return (
-     <div className={active ? 'homepage__imageblock active' : 'homepage__imageblock'}>{image[0] !== undefined ? <Img alt='displays images when hovering over text' fluid={image[0].fluid} /> : ''}</div>
+     <div className={active ? 'staticImageBlock active' : 'staticImageBlock'}>
+       <div className='staticImageBlock__container'>{image[0] !== undefined ? <Img alt='displays images when hovering over text' fluid={image[0].fluid} /> : ''}</div>
+     </div>
    )
   }} />
   )
