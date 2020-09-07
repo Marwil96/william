686147d204fceaa4d112af44a7b12435575df3cc
Marwil96 <william_martinsson@hotmail.com>
2020-09-07 13:19:02 +0200

    fresh

---
+++ gatsby-config.js
@@ -0,0 +1,19 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  siteMetadata: {
+    title: `William Martinsson`,
+    description: `William Martinsson DESC`,
+    author: `@William Martinsson`,
+    siteUrl: `https://williammartinsson.site`,
+  },
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-sass`
+  ],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const IndexPage =() => {
+  return (
+    <h3>Hey You All</h3>
+  )
+}
+
+export default IndexPage;
