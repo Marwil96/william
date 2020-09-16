c884a12fdc37671c74644f3b7f8319e64cce1920
Marwil96 <william_martinsson@hotmail.com>
2020-09-16 13:25:57 +0200

    Added Error Page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,38 @@
 import { Link } from 'gatsby';
 import React from 'react';
+import styled from 'styled-components';
+import { breakpoint } from "../mixins/breakpoint"
 import Button from '../components/Button';
 import PageWrapper from '../components/PageWrapper';
 
+const ErrorPageWrapper = styled.section`
+  max-width: 119rem;
+  display: flex;
+  flex-direction: column;
+  flex-direction: column;
+  padding: 20rem 0;
+  padding-bottom: 30rem;
+  width: 100%;
+  ${breakpoint.phone` 
+    padding: 15rem 1.6rem;
+    width: auto;
+  `}
+
+  h3 {
+    font-size: 6.4rem;
+    font-weight: 300;
+    margin-bottom: 3.2rem;
+  }
+`
 const ErrorPage = () => {
   return (
     <PageWrapper>
-      <h3>Sadness... Page is missing.</h3>
-      <Link to='/'><Button>Take me back</Button></Link>
+      <ErrorPageWrapper>
+        <h3>Sadness... Page is missing.</h3>
+        <Link to="/">
+          <Button>Take me back</Button>
+        </Link>
+      </ErrorPageWrapper>
     </PageWrapper>
   )
 }
