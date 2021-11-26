610c79647b12468d3662104cdbe7d64488e29371
Marwil96 <william_martinsson@hotmail.com>
2021-11-26 14:58:19 +0100

    add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,26 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { AnimatePresence } from 'framer-motion'
+import Script from 'next/script';
+import React from 'react';
 
 function MyApp({ Component, pageProps, router }: AppProps) {
-  return <AnimatePresence exitBeforeEnter> <Component {...pageProps} key={router.route} /></AnimatePresence>
+  return (
+  <React.Fragment>
+      <Script src="https://scripts.simpleanalyticscdn.com/latest.js" />
+      <noscript>
+        {/* eslint-disable @next/next/no-img-element */}
+        <img
+          src="https://queue.simpleanalyticscdn.com/noscript.gif"
+          alt=""
+          referrerPolicy="no-referrer-when-downgrade"
+        />
+      </noscript>
+    <AnimatePresence exitBeforeEnter>   
+      <Component {...pageProps} key={router.route} />
+    </AnimatePresence>
+  </React.Fragment>
+  )
 }
 
 export default MyApp
